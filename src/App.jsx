2812db1285d0291e35a810740e218e005bea7257
Adebisi Oluwarotimi <68323182+oluwarotimi1@users.noreply.github.com>
2022-12-17 19:51:15 +0100

    Navbar Page
    Navbar, Login and Signup Page Created

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import LoginPage from './pages/LoginPage'
+import NavbarPage from './pages/Navbar'
 
 function App() {
 
   return (
     <div className="App">
+      <NavbarPage />
       <LoginPage />
     </div>
   )
